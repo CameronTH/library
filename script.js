621e94a93bf285f265d1c6b6e532d6d111827893
CameronTH <cameronthomson40@gmail.com>
2023-11-03 21:22:32 +0000

    Completed the add book to library function

--- script.js
+++ script.js
@@ -7,32 +7,45 @@ const pages = document.querySelector("#pages");
 const bookContainer = document.querySelector("#book-container");
 const myForm = document.querySelector("#my-form");
 
-const myLibrary = ["Test", "Test2", "Test3"];
+const myLibrary = [];
 
-function Book() {}
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
 
-function addBookToLibrary() {}
+function addBookToLibrary() {
+  const newBook = new Book(title.value, author.value, pages.value);
+  myLibrary.push(newBook);
+  bookContainer.replaceChildren();
+  myLibrary.forEach((element) => {
+    const bookDiv = document.createElement("div");
+    const bookTitle = document.createElement("h2");
+    const bookAuthor = document.createElement("p");
+    const bookPages = document.createElement("p");
+    bookDiv.classList.add("book");
+    bookTitle.textContent = element.title;
+    bookAuthor.textContent = `Author: ${element.author}`;
+    bookPages.textContent = `Pages: ${element.pages}`;
+    bookDiv.appendChild(bookTitle);
+    bookDiv.appendChild(bookAuthor);
+    bookDiv.appendChild(bookPages);
+    bookContainer.append(bookDiv);
+  });
+}
 
 addBookButton.addEventListener("click", () => {
   console.log("Pressed");
-  form.style.display = "flex";
+  myForm.style.display = "flex";
 });
 
 formAdd.addEventListener("click", (event) => {
-  const bookDiv = document.createElement("div");
-  const bookTitle = document.createElement("h2");
-  const bookAuthor = document.createElement("p");
-  const bookPages = document.createElement("p");
-  bookDiv.classList.add("book");
-  bookTitle.textContent = title.value;
-  bookAuthor.textContent = `Author: ${author.value}`;
-  bookPages.textContent = `Pages: ${pages.value}`;
-  bookDiv.appendChild(bookTitle);
-  bookDiv.appendChild(bookAuthor);
-  bookDiv.appendChild(bookPages);
-
-  if (title.value.length != 0 || author.value.length != 0 || pages.value != 0) {
-    bookContainer.append(bookDiv);
+  if (title.value.length != 0 && author.value.length != 0 && pages.value != 0) {
+    addBookToLibrary();
+    event.preventDefault();
+    myForm.style.display = "none";
+    myForm.reset();
+    console.log(myLibrary);
   }
-  event.preventDefault();
 });
