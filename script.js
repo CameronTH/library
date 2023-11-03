5981050b52cb78003bd5f3ba3d7d329c0c7e45a4
CameronTH <cameronthomson40@gmail.com>
2023-11-03 18:16:01 +0000

    improved the add book function and added some validation to the form

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const addBookButton = document.querySelector("#button");
 const form = document.querySelector(".form");
 const formAdd = document.querySelector(".form-button");
-const author = document.querySelector(".author");
-const title = document.querySelector(".title");
-const pages = document.querySelector(".pages");
+const author = document.querySelector("#author");
+const title = document.querySelector("#title");
+const pages = document.querySelector("#pages");
 const bookContainer = document.querySelector("#book-container");
+const myForm = document.querySelector("#my-form");
 
 const myLibrary = ["Test", "Test2", "Test3"];
 
@@ -15,23 +16,23 @@ function addBookToLibrary() {}
 addBookButton.addEventListener("click", () => {
   console.log("Pressed");
   form.style.display = "flex";
+});
+
+formAdd.addEventListener("click", (event) => {
   const bookDiv = document.createElement("div");
   const bookTitle = document.createElement("h2");
   const bookAuthor = document.createElement("p");
   const bookPages = document.createElement("p");
   bookDiv.classList.add("book");
-  bookTitle.textContent = "Testing Append";
-  bookAuthor.textContent = "Author: Someone";
-  bookPages.textContent = "Pages: 0";
+  bookTitle.textContent = title.value;
+  bookAuthor.textContent = `Author: ${author.value}`;
+  bookPages.textContent = `Pages: ${pages.value}`;
   bookDiv.appendChild(bookTitle);
   bookDiv.appendChild(bookAuthor);
   bookDiv.appendChild(bookPages);
-  bookContainer.append(bookDiv);
-});
 
-formAdd.addEventListener("click", () => {
-  console.log(author.value);
-  console.log(title.value);
-  console.log(pages.value);
-  bookContainer.style.display = "none";
+  if (title.value.length != 0 || author.value.length != 0 || pages.value != 0) {
+    bookContainer.append(bookDiv);
+  }
+  event.preventDefault();
 });
