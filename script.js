f7ac54b54f41fba04017814619bf31be180edda0
CameronTH <cameronthomson40@gmail.com>
2023-11-19 16:54:55 +0000

    Changed object constructor to a class

--- script.js
+++ script.js
@@ -11,11 +11,13 @@ const dialog = document.querySelector("#dialog");
 
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }
 
 function addBookToLibrary() {
